40b27c9c0e3d67f6894c6aeccc0f95d94368318e
Kamila Moraes <kamilaamora336@gmail.com>
2026-08-08 20:55:41 -0300

    Criar menu inicial da biblioteca

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("===== LIBRARY MANAGER =====");
+        System.out.println("1 - Verificar Livro");
+        System.out.println("2 - Sair");
+        System.out.print("Escolha uma opção: ");
+
+        int opcao = scanner.nextInt();
+
+
+    }
+}
