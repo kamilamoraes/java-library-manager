a37db0308aa0efa3fe93f5bcfd6a4ebafbc7c67c
Kamila Moraes <kamilaamora336@gmail.com>
2026-08-12 22:23:35 -0300

    Adicionar repetição do menu com do while

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,25 @@ public class Main {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("===== LIBRARY MANAGER =====");
-        System.out.println("1 - Verificar Livro");
-        System.out.println("2 - Sair");
-        System.out.print("Escolha uma opção: ");
+        int opcao;
+    
+        do {
+            System.out.println("===== LIBRARY MANAGER =====");
+            System.out.println("1 - Verificar Livro");
+            System.out.println("2 - Sair");
+            System.out.print("Escolha uma opção: ");
 
-        int opcao = scanner.nextInt();
+            opcao = scanner.nextInt();
 
-        if (opcao == 1) {
-            System.out.println("Você escolheu verificar um livro.");
-        } else if (opcao == 2) {
-            System.out.println("Saindo do Library Manager...");
-        } else {
-            System.out.println("Opção inválida!");
-        }
+            if (opcao == 1) {
+                System.out.println("Você escolheu verificar um livro.");
+            } else if (opcao == 2) {
+                System.out.println("Saindo do Library Manager...");
+            } else {
+                System.out.println("Opção inválida!");
+            }
 
+        } while (opcao != 2);
 
     }
 }
